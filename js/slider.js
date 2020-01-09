b6d7c6805fff3800d32984093826d7d6e7c5e550
unknown <huangherman41@gmail.com>
2020-01-09 18:50:10 +0700

    FE Dev tools

---
+++ js/slider.js
@@ -0,0 +1,7 @@
+$('.category-list').slick({
+    infinite: false,
+    slidesToShow: 6,
+    slidesToScroll: 3,
+    nextArrow: '<i class="fas fa-caret-right next-arrow"></i>',
+    prevArrow: '<i class="fas fa-caret-left prev-arrow"></i>'
+});
